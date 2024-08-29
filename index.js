936d750ea81b10e18aad7e634e7d7451409e83a1
gracenkazi <pleina1@yahoo.fr>
2024-08-29 11:35:53 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//import {Stack} from './stack.js';
+//import stack from './stack.js';
+import * as Stack from './stack.js';
+
+const stack = new Stack.Stack();
+/***OR ***/
+//const stack = new Stack();
+
+//Stack.push(11)
+
+console.log(stack.size());
+
+const lastItem = stack.peek();
+console.log(lastItem);
+
+
+
+
